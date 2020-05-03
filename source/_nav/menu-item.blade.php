34321b1691d6ad91d6606095d3f83b154a9ab79f
CyberSai <isaacsai030@gmail.com>
2020-05-03 11:53:31 +0000

    Navigation link fixed

--- source/_nav/menu-item.blade.php
+++ source/_nav/menu-item.blade.php
@@ -1,7 +1,7 @@
 <li class="pl-4">
     @if ($url = is_string($item) ? $item : $item->url)
         {{-- Menu item with URL--}}
-        <a href="{{ $page->url($url) }}"
+        <a href="{{ $page->production ? '/ussd-docs' : '' }}{{ $page->url($url) }}"
             class="{{ 'lvl' . $level }} {{ $page->isActiveParent($item) ? 'lvl' . $level . '-active' : '' }} {{ $page->isActive($url) ? 'active font-semibold text-blue-500' : '' }} nav-menu__item hover:text-blue-500"
         >
             {{ $label }}
